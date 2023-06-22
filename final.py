e115e033db0fb325b9f666a78a2f388705c051a5
sinha-rakshit <sinha.rakshit.16@gmail.com>
2023-06-22 12:59:49 +0530

    added update option in final.py and also found some errors in the schema

--- final.py
+++ final.py
@@ -24,3 +24,42 @@ elif choice == 2:
     sqlValues = (userID,)  # tuple cannot be created with a comma
     mycursor.execute(sqlFormula, sqlValues)
     mydb.commit()
+
+elif choice == 3:
+    choiceUpdate = int(input("Enter the choice:"))
+    if choiceUpdate == 1:
+        userID_old = input("Enter the userID(old):")
+        userID_new = input("Enter the userID(new):")
+        sqlFormula = "UPDATE Task_Manager SET userID=%s WHERE userID=%s"
+        sqlValues = (userID_new, userID_old)
+        mycursor.execute(sqlFormula, sqlValues)
+        mydb.commit()
+
+    elif choiceUpdate == 2:
+        userID = input("Enter the userID:")
+        name_old = input("Enter your name(old):")
+        name_new = input("Enter your name(new):")
+        sqlFormula = "UPDATE Task_Manager SET NAME=%s WHERE userID=%s AND NAME=%s"
+        sqlValues = (name_new, userID, name_old)
+        mycursor.execute(sqlFormula, sqlValues)
+        mydb.commit()
+
+    elif choiceUpdate == 3:
+        userID = input("Enter the userID:")
+        task_old = input("Enter your Task(old):")
+        task_new = input("Enter your Task(new):")
+        sqlFormula = "UPDATE Task_Manager SET TASK=%s WHERE userID=%s AND TASK=%s"
+        sqlValues = (task_new, userID, task_old)
+        mycursor.execute(sqlFormula, sqlValues)
+        mydb.commit()
+
+    elif choiceUpdate == 4:
+        userID = input("Enter the userID:")
+        Deadline_old = input("Enter your Deadline(old):")
+        Deadline_new = input("Enter your Dealine(new):")
+        sqlFormula = (
+            "UPDATE Task_Manager SET DEADLINE=%s WHERE userID=%s AND DEADLINE=%s"
+        )
+        sqlValues = (Deadline_new, userID, Deadline_old)
+        mycursor.execute(sqlFormula, sqlValues)
+        mydb.commit()
