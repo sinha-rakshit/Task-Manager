a515c487f6a43eeef5af46fe34bf732320991124
sinha-rakshit <sinha.rakshit.16@gmail.com>
2023-06-22 12:36:24 +0530

    added delete option in final

--- final.py
+++ final.py
@@ -10,10 +10,17 @@ if choice == 1:
     userID = input("Enter Your UserID(Eg. rak123):")
     name = input("Enter Your Name:")
     Task = input("Enter the Task:")
-    DeadLine = input("Enter the DeadLine:")
+    DeadLine = input("Enter the DeadLine(Eg. YYYY-MM-DD):")
     sqlFormula = (
         "INSERT INTO Task_Manager(userID,NAME,TASK,DEADLINE) VALUES (%s,%s,%s,%s)"
     )
     sqlValues = (userID, name, Task, DeadLine)
+    mycursor.execute(sqlFormula, sqlValues)
+    mydb.commit()
+
+elif choice == 2:
+    userID = input("Enter Your USERID(Eg. rak123):")
+    sqlFormula = "DELETE FROM Task_Manager WHERE userID =%s"
+    sqlValues = (userID,)  # tuple cannot be created with a comma
     mycursor.execute(sqlFormula, sqlValues)
     mydb.commit()
